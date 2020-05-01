722d6175cd3649242c86d4eb3a1ceefafeed8e82
Divya Tagtachian <hello@shortdiv.com>
2020-05-01 15:04:21 -0500

    added netlify form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style lang="scss">
+ul {
+  list-style: none;
 }
-</script>
-
-<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body,
+html {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+a {
+  text-decoration: none;
+}
+button {
+  width: 100%;
+  padding: 20px 60px;
+  outline: none;
+  background-color: #282c34;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 9px #95a5a6;
+  font-size: 1.2em;
+  color: white;
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <PollForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PollForm from '@/components/PollForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    PollForm
+  }
+}
+</script>
